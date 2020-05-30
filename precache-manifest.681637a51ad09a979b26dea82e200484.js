e3c99d79746815facc2caa6dc9d3ba406c845e57
Simon Defrenet <simondefrenet@Simons-MacBook-Pro.local>
2020-05-30 18:32:31 -0400

    Updates

---
+++ precache-manifest.681637a51ad09a979b26dea82e200484.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aee2da32461b71e0af8ce8220bd6e58f",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "6731f1f3300ac4ba9a3c",
+    "url": "/my-portfolio/static/css/main.c0710856.chunk.css"
+  },
+  {
+    "revision": "58caa0dddf7d3e2c27d5",
+    "url": "/my-portfolio/static/js/2.f96777e2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-portfolio/static/js/2.f96777e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6731f1f3300ac4ba9a3c",
+    "url": "/my-portfolio/static/js/main.96c085a0.chunk.js"
+  },
+  {
+    "revision": "170f45050b91094c3a2e",
+    "url": "/my-portfolio/static/js/runtime-main.354f34ae.js"
+  }
+]);
